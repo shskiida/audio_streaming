86e1dbc25f780f4ab8abda8950fbc6ddcbd96498
shskiida <83195110+shskiida@users.noreply.github.com>
2021-08-19 11:51:15 +0900

    Add files via upload

---
+++ audio_streaming.py
@@ -0,0 +1,135 @@
+#ストリーミング加工
+#音声の周波数成分可視化(カットオフ2)
+#FFT
+#周波数カットオフ
+#逆FFT
+#保存
+
+import requests
+import sys
+import scipy.io.wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import wave
+import os
+from scipy.io.wavfile import write
+import pyaudio
+
+#録音する時間の長さ（秒）
+recsec = 10
+iDeviceIndex = 0 #録音デバイスのインデックス番号
+ 
+FORMAT = pyaudio.paInt16 #音声のフォーマット
+CHANNELS = 1             #モノラル
+RATE = 44100             #サンプルレート
+CHUNK = 2**11            #データ点数
+audio = pyaudio.PyAudio() #pyaudio.PyAudio()
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+        rate=RATE, input=True, output=True,
+        input_device_index = iDeviceIndex, 
+        frames_per_buffer=CHUNK)
+frames = []
+for i in range(0, int(RATE / CHUNK * recsec)):
+    rawdata = stream.read(CHUNK)
+    frames.append(rawdata)
+a = np.array(frames)    
+
+ndarray = np.frombuffer(a, dtype='int16')
+
+#（振幅）の配列を作成
+data = ndarray / 32768
+
+time = np.arange(0, data.shape[0]/rate, 1/rate)
+#データプロット
+plt.plot(time, data)
+plt.ylim(-1.0,1.0)
+plt.xlabel('time(s)')
+plt.ylabel('amplitude')
+plt.title('streaming_wave')
+plt.show()
+
+
+#FFTにより周波数成分を表示する。
+#縦軸：dataを高速フーリエ変換する（時間領域から周波数領域に変換する）
+F = np.fft.fft(data)
+fft_data = np.abs(F)
+
+#横軸：周波数の取得　　#np.fft.fftfreq(データ点数, サンプリング周期)
+freqList = np.fft.fftfreq(data.shape[0], d=1.0/rate)
+
+#データプロット
+plt.plot(freqList, fft_data)
+plt.ylim(0,2000)
+plt.xlim(0,6000) #0～6000Hzまで表示
+plt.xlabel('Frequency[Hz]')
+plt.ylabel('amplitude')
+plt.title('streaming_wave')
+plt.show()
+
+
+#周波数指定してフィルタリング
+F_cut = np.copy(F)
+
+fcmaxU = 2200 #上限カットオフ周波数
+fcmaxL = -fcmaxU 
+fcminU = 100 #下限カットオフ周波数
+fcminL = -fcminU
+fc1 = 135
+fc2 = 137
+F_cut[(freqList > fcmaxU) | (freqList < fcmaxL)] = 0 #カットオフ周波数のデータを0にする。
+F_cut[(freqList > fcminL) & (freqList < fcminU)] = 0 #カットオフ周波数のデータを0にする。
+F_cut[(freqList > fc1) & (freqList < fc2)] = 0 #カットオフ周波数以下のデータを0にする。
+
+fft_data_cut = np.abs(F_cut)
+
+
+#max,aveの算出
+max_data = round(max(fft_data_cut),1)
+nonzerodata = np.count_nonzero(fft_data_cut) #fft_data_cutの中から0ではないデータ個数のカウント
+ave_data = round(sum(fft_data_cut)/nonzerodata,1) #平均値の算出
+print(f'max={max_data}')
+print(f'ave={ave_data}')
+
+#振幅によってスコアを決める。
+if ave_data < 20 :
+    score = 1    
+elif ave_data >= 20 and ave_data < 40 :
+    score = 2
+elif ave_data >= 40 and ave_data < 60 :
+    score = 3
+elif ave_data >= 60 and ave_data < 80 :
+    score = 4
+elif ave_data >=80 :
+    score = 5
+
+print(f'score={score}')
+
+#データプロット
+plt.plot(freqList, fft_data_cut)
+plt.ylim(0,2000)
+plt.xlim(0,6000) #0～6000Hzまで表示
+plt.xlabel('Frequency[Hz]')
+plt.ylabel('amplitude')
+plt.title('streaming_wave')
+plt.show()
+
+#逆FFT
+newF = np.fft.ifft(F_cut)
+
+time = np.arange(0, newF.shape[0]/rate, 1/rate)  
+#データプロット
+plt.plot(time, newF)
+plt.ylim(-1.0,1.0)
+plt.xlabel('time(s)')
+plt.ylabel('amplitude')
+plt.title('streaming_wave')
+plt.show()
+
+
+#処理後データの保存
+#newF = newF*32768
+#newFi = newF.real.astype(np.int16)
+#write('sample.wav', rate, newFi)
+#print("処理後のデータを保存しました。")
